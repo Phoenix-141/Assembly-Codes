fdfbddbca8a9cbe88c5002485567783796674393
unknown <gui.ribs2000#gmail.com>
2022-04-17 03:08:31 -0300

    Hello World

--- HelloWorld/HelloWorld.asm
+++ HelloWorld/HelloWorld.asm
@@ -1,4 +1,6 @@
-section .data                               
+section .data  
+    sms: db 'Hello World !!!!', 0xA
+    tam equ $- sms                          
 
 section .bss                                
 
@@ -7,7 +9,15 @@ section .text
 global _start                              
 
 
-_EnterProgram:                             
+_EnterProgram:  
+    mov RAX, 0x4
+    mov RBX, 0x1
+    mov RCX, sms
+    mov RDX, tam
+    int  0x80  
+
+
+_Break:
     mov  RAX, 0x1
     mov  RBX, 0x0       
     int  0x80                      
